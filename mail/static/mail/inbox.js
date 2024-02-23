d184742c68a9c7485532657ed0c5ef3d132a18e1
odakris <christopher_denis@live.fr>
2024-02-23 17:31:19 +0100

    handle send mail

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -7,6 +7,12 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // By default, load the inbox
   load_mailbox("inbox");
+
+  // Handle sending mails form
+  document.querySelector("#compose-form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    send_email();
+  });
 });
 
 function compose_email() {
@@ -27,4 +33,23 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector("#emails-view").innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+}
+
+function send_email() {
+  // Send mail
+  fetch("/emails", {
+    method: "POST",
+    body: JSON.stringify({
+      recipients: document.querySelector("#compose-recipients").value,
+      subject: document.querySelector("#compose-subject").value,
+      body: document.querySelector("#compose-body").value,
+    }),
+  })
+    .then((response) => response.json())
+    .then((result) => {
+      console.log(result);
+      // redirect to sent messages
+      load_mailbox("sent");
+    })
+    .catch((error) => console.log(error));
 }
