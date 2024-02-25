7c2b1385505585fc0697c388384ce2ed2226fc9d
odakris <christopher_denis@live.fr>
2024-02-25 14:53:51 +0100

    handle loading mails

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -10,6 +10,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // Handle sending mails form
   document.querySelector("#compose-form").addEventListener("submit", (e) => {
+    // Prevent form submittion
     e.preventDefault();
     send_email();
   });
@@ -33,6 +34,56 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector("#emails-view").innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+  // Fetch 'mailbox' mails
+  fetch("/emails/" + mailbox)
+    .then((response) => response.json())
+    .then((emails) => {
+      const emailsView = document.getElementById("emails-view");
+
+      emails.forEach((email) => {
+        // Define email block
+        const blockMail = document.createElement("div");
+        blockMail.setAttribute("id", email.id);
+        blockMail.classList.add("email", "flex");
+
+        // Define sender / receiver div
+        if (mailbox == "sent") {
+          const receiver = document.createElement("div");
+          receiver.classList.add("receiver");
+
+          if (email.recipients.length > 1) {
+            // If more than one receiver
+            receiver.innerHTML = "To: " + email.recipients[0] + " and more..";
+          } else {
+            // If only one receiver
+            receiver.innerHTML = "To: " + email.recipients;
+          }
+
+          blockMail.append(receiver);
+        } else {
+          const sender = document.createElement("div");
+          sender.classList.add("sender");
+          sender.innerHTML = "From: " + email.sender;
+          blockMail.append(sender);
+        }
+
+        // Define subject div
+        const subject = document.createElement("div");
+        subject.classList.add("subject");
+        subject.innerHTML = email.subject;
+        blockMail.append(subject);
+
+        // Define time div
+        const time = document.createElement("div");
+        time.classList.add("time");
+        time.innerHTML = email.timestamp;
+        blockMail.append(time);
+
+        // Append new email
+        emailsView.append(blockMail);
+      });
+    });
 }
 
 function send_email() {
